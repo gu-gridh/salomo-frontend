c50b5a9e83fc4d27b72821d8448d74543473a826
Tristan Bridge <tbridge360@gmail.com>
2026-06-24 11:06:24 -0700

    new addition to camera path

--- src/cameraPath.js
+++ src/cameraPath.js
@@ -3,4 +3,5 @@ export const cameraPath = [
     { time: 0.0, position: [0, 0.1, 1.1] },
     { time: 5.0, position: [1.2, 0.1, 0.8] },
     { time: 10.0, position: [0, 0.5, -1.2] },
+    { time: 20.0, position: [2.0, 1.5, 1.0] },
 ]
